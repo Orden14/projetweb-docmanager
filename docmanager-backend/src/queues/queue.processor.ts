d3470e7e8fa389940e410d722fae725825c81680
Orden14 <orden0614@gmail.com>
2025-06-19 13:12:52 +0200

    doc created log queue

--- docmanager-backend/src/queues/queue.processor.ts
+++ docmanager-backend/src/queues/queue.processor.ts
@@ -7,4 +7,14 @@ export class QueueProcessor {
     async handleHealthCheckJob(job: Job): Promise<void> {
         console.log('Processing job:', job.data);
     }
+
+    @Process('documentCreated')
+    async handleDocumentCreated(job: Job): Promise<void> {
+        console.log('Document created:', job.data.document);
+    }
+
+    @Process('documentDeleted')
+    async handleDocumentDeleted(job: Job): Promise<void> {
+        console.log('Document deleted:', job.data.document);
+    }
 }
--- docmanager-backend/src/services/document.service.ts
+++ docmanager-backend/src/services/document.service.ts
@@ -1,13 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { Document } from '../entities/document.entity';
+import { DocumentQueueService } from '../queues/document.queue';
 
 @Injectable()
 export class DocumentService {
     private readonly documents: Document[] = [];
 
+    constructor(private readonly documentQueueService: DocumentQueueService) {}
+
     create(document: Document): Document {
         this.documents.push(document);
+        this.documentQueueService.addJob('documentCreated', { document })
+
         return document;
+    }
+
+    delete(documentId: string): boolean {
+        const index = this.documents.findIndex(doc => doc.id === documentId);
+        if (index === -1) return false;
+
+        const [deletedDocument] = this.documents.splice(index, 1);
+        this.documentQueueService.addJob('documentDeleted', { document: deletedDocument });
+
+        return true;
     }
 
     findAll(): Document[] {
